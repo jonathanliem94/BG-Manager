b87a240b6c33118610339322308bfdbf7700ebf2
ftt.jonathan.liem <ftt.jonathan.liem@grabtaxi.com>
2020-01-16 15:57:01 +0800

    Added extra settings fragment Removed FAB Removed toolbar Found out how to remove the action without breaking the navigation drawer

---
+++ app/src/androidTest/java/com/jonathanl/bgmanager/MainMenuNavigationTest.kt
@@ -0,0 +1,15 @@
+package com.jonathanl.bgmanager
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainMenuNavigationTest {
+
+    @Test
+    fun clickOnMenuIconWhileInMainPage() {
+        
+    }
+
+}
--- app/src/main/java/com/jonathanl/bgmanager/MainActivity.kt
+++ app/src/main/java/com/jonathanl/bgmanager/MainActivity.kt
@@ -21,14 +21,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -40,6 +36,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_gallery,
                 R.id.nav_slideshow,
                 R.id.nav_tools,
+                R.id.nav_settings,
                 R.id.nav_share,
                 R.id.nav_send
             ), drawerLayout
@@ -48,14 +45,9 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
+
