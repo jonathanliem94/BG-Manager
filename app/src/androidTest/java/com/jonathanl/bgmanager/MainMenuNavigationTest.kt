12120f59e4f77cf13f849b51ee832d216f800430
ftt.jonathan.liem <ftt.jonathan.liem@grabtaxi.com>
2020-01-17 18:43:54 +0800

    Added some UI tests, and modified CI/CD for UI tests

--- app/src/androidTest/java/com/jonathanl/bgmanager/MainMenuNavigationTest.kt
+++ app/src/androidTest/java/com/jonathanl/bgmanager/MainMenuNavigationTest.kt
@@ -1,15 +1,40 @@
 package com.jonathanl.bgmanager
 
+import android.view.Gravity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainMenuNavigationTest {
 
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun clickOnMenuIconWhileInMainPage() {
-        
+    fun openAndCloseNavigationDrawerWhileInMainPage() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.close())
+    }
+
+    @Test
+    fun navigateToGameList() {
+        onView(withId(R.id.drawer_layout))
+            .check(matches(isClosed(Gravity.LEFT)))
+            .perform(DrawerActions.open())
+
+        onView(withId(R.id.nav_view))
+            .perform(NavigationViewActions.navigateTo(R.id.nav_game_list))
+
+        onView(withId(R.id.text_gamelist)).check(matches(isDisplayed()))
     }
 
 }
